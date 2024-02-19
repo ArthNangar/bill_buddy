b9ada3ad2dac635ee11ecfcc621d8ec89e4698b2
Arth7 <arthnangar1@gmail.com>
2024-02-19 21:48:30 +0530

    Updated the invoice Adding

--- client/src/components/AddInvoice.jsx
+++ client/src/components/AddInvoice.jsx
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled";
 import {  TextField,Box } from "@mui/material";
+import React , {useState} from "react";
+import { saveInvoice } from "../services/api";
 
 const Component = styled(Box)(
     {
@@ -13,24 +15,49 @@ const Component = styled(Box)(
     }
 )
 
+const defaultObj = {
+    vendor:'',
+    product:'',
+    amount: 0,
+    date:'',
+    action:'pending'
+}
 const AddInvoice =() => {
+    const[invoice , setInvoice] = useState( defaultObj);
+    const onValuechange = (e) => {
+        setInvoice({...invoice, [e.target.name]: e.target.value});
+        
+    }
+
+    const addNewInvoice =async () =>{
+       await  saveInvoice({...invoice, amount: Number(invoice['amount'])});
+    }
+
     return(
         <Component>
             <h3 style={{ textAlign: 'left', marginLeft: 20 }}>Pending Bills List:</h3>
             <Box>
             <TextField 
                 placeholder="Enter the name of Buyer"
+                onChange={ (e) => onValuechange(e)}
+                name="vendor"
             />
             <TextField 
                 placeholder="Enter the product Name"
+                onChange={ (e) => onValuechange(e)}
+                name="product"
             />
             <TextField 
                 placeholder="Enter the Amount remaing"
                 type="number"
+                onChange={ (e) => onValuechange(e)}
+                name="amount"
             />
             <TextField 
                 placeholder="Enter the purchase Date "
                 type="date"
+                onChange={ (e) => onValuechange(e)}
+                name="date"
             />
             <button style={{
              backgroundColor: '#e74c3c', // Adjust color as needed
@@ -42,7 +69,9 @@ const AddInvoice =() => {
              fontSize: '16px',
              fontWeight: 'bold',
              }
-            }>
+            }
+                onClick={() => addNewInvoice()}
+            >
             Add Bill
             </button>
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,27 @@
 import Header from "./Header";
 import './button.css';
 import AddInvoice from "./AddInvoice";
+import React, {useState} from "react";
 
 
 const Home = () =>{
+
+    const [addInvoice, setAddInvoice] = useState(false);
+
+    const toggleInvoice =() =>{
+        setAddInvoice(true);
+    }
     return(
        <>
        <Header />
        <div>
        <h1 style={{color:"black"}}> Pending Invoice</h1>
        {/* <button style={{ background: 'blue', color: 'white', padding: '10px' }}>Add Invoice </button> */}
-       <button className="gradient-button">Add Invoice</button>
-       <AddInvoice />
+       { !addInvoice && <button className="gradient-button"
+       onClick={() => toggleInvoice() }
+       >Add Invoice</button>
+       }
+       { addInvoice && <AddInvoice />}
 
 
        </div>
